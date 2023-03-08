868c24e488e99f0fa4ceed307ff2c0ec1dced9ad
Karan Ahlawat <ahlawatkaran12@gmail.com>
2023-03-08 19:48:11 +0530

    [add] figure out baseGrammar

--- grammar.js
+++ grammar.js
@@ -1,7 +1,21 @@
-module.exports = grammar({
-	name: 'Blade',
-	
-	rules: {
-		source_file: $ => 'hello'
-	}
+const html = require("./tree-sitter-html/grammar");
+
+// [NOTE] Verbatim missing for now
+const echo_opening_tags = [
+	"{{{",
+	"{{",
+	"{!!",
+	"{{--"
+]
+
+const echo_closing_tags = [
+	"}}}",
+	"}}",
+	"!}}",
+	"--}}"
+]
+
+module.exports = grammar(html, {
+	name: "blade",
+	rules: {}
 });
